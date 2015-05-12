cfb50b9ee60b526675dff8ad97b89396fe5e2610
Vladislav Slesarenko <vslesarenko@oggettoweb.com>
2015-05-12 16:28:49 +0300

    Add tests for calculating price in api model for different input data

--- app/code/local/Oggetto/Shipping/Test/Model/Api/Shipping.php
+++ app/code/local/Oggetto/Shipping/Test/Model/Api/Shipping.php
@@ -32,13 +32,12 @@
  */
 class Oggetto_Shipping_Test_Model_Api_Shipping extends EcomDev_PHPUnit_Test_Case
 {
-
     /**
      * Return prices calculated by oggetto shipping API
      *
      * @param array  $orig         origin location
      * @param array  $dest         destination location
-     * @param string $responseBody response body
+     * @param string $responseBody response body with prices
      *
      * @return void
      *
@@ -46,8 +45,87 @@ class Oggetto_Shipping_Test_Model_Api_Shipping extends EcomDev_PHPUnit_Test_Case
      */
     public function testReturnsPricesCalculatedByOggettoShippingApi($orig, $dest, $responseBody)
     {
-        $httpClientMock = $this->getMock('Varien_Http_Client', ['resetParameters', 'setParameterGet', 'request']);
+        $httpResponseMock = $this->
+                    _getHttpResponseMockWithGetStatusAndBodyMethodsAndDisabledConstructor(200, $responseBody);
 
+        $httpClientMock = $this->
+                    _getHttpClientMockWithResetAndSetParametersMethodsAndRequest($httpResponseMock);
+
+        $apiMock = $this->
+                    _getOggettoShippingApiMockWithProtectedGetHttpClientMethodAndReplaceIt($httpClientMock);
+
+        $this->assertEquals(
+            $this->expected()->getData()['calculated_prices'],
+            $apiMock->calculatePrices($orig, $dest)
+        );
+    }
+
+    /**
+     * Return error message when input data is invalid from calculating prices by oggetto shipping API
+     *
+     * @param array  $orig         origin location
+     * @param array  $dest         destination location
+     * @param string $responseBody response body with error
+     *
+     * @return void
+     *
+     * @dataProvider dataProvider
+     */
+    public function testReturnsErrorMessageFromCalculatingPricesByOggettoShippingApi($orig, $dest, $responseBody)
+    {
+        //var_dump($this->expected()->getData()['error_message']);die;
+
+        $httpResponseMock = $this->
+                    _getHttpResponseMockWithGetStatusAndBodyMethodsAndDisabledConstructor(200, $responseBody);
+
+        $httpClientMock = $this->
+                    _getHttpClientMockWithResetAndSetParametersMethodsAndRequest($httpResponseMock);
+
+        $apiMock = $this->
+                    _getOggettoShippingApiMockWithProtectedGetHttpClientMethodAndReplaceIt($httpClientMock);
+
+        $this->assertEquals(
+            $this->expected()->getData()['error_message'],
+            $apiMock->calculatePrices($orig, $dest)
+        );
+    }
+
+    /**
+     * Return empty array when response is not 200(OK) from calculating prices by oggetto shipping API
+     *
+     * @param array  $orig         origin location
+     * @param array  $dest         destination location
+     *
+     * @return void
+     *
+     * @dataProvider dataProvider
+     */
+    public function testReturnsEmptyArrayFromCalculatingPricesByOggettoShippingApi($orig, $dest)
+    {
+        $httpResponseMock = $this->
+                _getHttpResponseMockWithGetStatusAndBodyMethodsAndDisabledConstructor(777);
+
+        $httpClientMock = $this->
+                _getHttpClientMockWithResetAndSetParametersMethodsAndRequest($httpResponseMock);
+
+        $apiMock = $this->_getOggettoShippingApiMockWithProtectedGetHttpClientMethodAndReplaceIt($httpClientMock);
+
+        $this->assertEquals(
+            [],
+            $apiMock->calculatePrices($orig, $dest)
+        );
+    }
+
+    /**
+     * Get Http Response mock with get status and body methods and disabled constructor
+     *
+     * @param int    $status expected response status
+     * @param string $body   expected response body
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function _getHttpResponseMockWithGetStatusAndBodyMethodsAndDisabledConstructor($status, $body = null)
+    {
         $httpResponseMock = $this->getMockBuilder('Zend_Http_Response')
             ->disableOriginalConstructor()
             ->setMethods(['getStatus', 'getBody'])
@@ -55,11 +133,30 @@ class Oggetto_Shipping_Test_Model_Api_Shipping extends EcomDev_PHPUnit_Test_Case
 
         $httpResponseMock->expects($this->once())
             ->method('getStatus')
-            ->willReturn(200);
+            ->willReturn($status);
 
-        $httpResponseMock->expects($this->once())
-            ->method('getBody')
-            ->willReturn($responseBody);
+        if (is_null($body)) {
+            $httpResponseMock->expects($this->never())
+                ->method('getBody');
+        } else {
+            $httpResponseMock->expects($this->once())
+                ->method('getBody')
+                ->willReturn($body);
+        }
+
+        return $httpResponseMock;
+    }
+
+    /**
+     * Get Http Client mock with reset and set parameters methods and request
+     *
+     * @param PHPUnit_Framework_MockObject_MockObject $httpResponseMock Http Response Mock
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function _getHttpClientMockWithResetAndSetParametersMethodsAndRequest($httpResponseMock)
+    {
+        $httpClientMock = $this->getMock('Varien_Http_Client', ['resetParameters', 'setParameterGet', 'request']);
 
         $httpClientMock->expects($this->once())
             ->method('resetParameters')
@@ -74,6 +171,18 @@ class Oggetto_Shipping_Test_Model_Api_Shipping extends EcomDev_PHPUnit_Test_Case
             ->with('GET')
             ->willReturn($httpResponseMock);
 
+        return $httpClientMock;
+    }
+
+    /**
+     * Get Oggetto Shipping Api mock with protected getHttpClient method and replace model by this mock
+     *
+     * @param PHPUnit_Framework_MockObject_MockObject $httpClientMock Http Client Mock
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function _getOggettoShippingApiMockWithProtectedGetHttpClientMethodAndReplaceIt($httpClientMock)
+    {
         $apiMock = $this->getModelMock('oggetto_shipping/api_shipping', ['_getHttpClient']);
 
         $apiMock->expects($this->once())
@@ -82,9 +191,6 @@ class Oggetto_Shipping_Test_Model_Api_Shipping extends EcomDev_PHPUnit_Test_Case
 
         $this->replaceByMock('model', 'oggetto_shipping/api_shipping', $apiMock);
 
-        $this->assertEquals(
-            $this->expected()->getData()['calculated_prices'],
-            $apiMock->calculatePrices($orig, $dest)
-        );
+        return $apiMock;
     }
 }
