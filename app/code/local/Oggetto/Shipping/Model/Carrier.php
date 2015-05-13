e6591978073d6be3aebe22fed861ffef19f5db41
Vladislav Slesarenko <vslesarenko@oggettoweb.com>
2015-05-13 17:11:35 +0300

    Add collectRates() method in carrier model

--- app/code/local/Oggetto/Shipping/Model/Carrier.php
+++ app/code/local/Oggetto/Shipping/Model/Carrier.php
@@ -40,6 +40,10 @@ class Oggetto_Shipping_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstrac
      */
     protected $_code = 'oggetto_shipping';
 
+    protected $_countryNames = [
+        'RU' => 'Россия',
+    ];
+
     /**
      * Returns active status from config
      *
@@ -58,6 +62,48 @@ class Oggetto_Shipping_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstrac
      */
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
+        if (!$this->isActive()) {
+            return false;
+        }
+
+        $origin = $this->_getOriginAddress();
+        $origin['country'] = $this->_getCountryNameById($origin['country']);
+        $origin['region'] = $this->_getRegionNameById($origin['region']);
+
+        $destination = $this->_getDestinationAddress($request);
+        $destination['country'] = $this->_getCountryNameById($destination['country']);
+        $destination['region'] = $this->_getRegionNameById($destination['region']);
+
+        /** @var Oggetto_Shipping_Model_Api_Shipping $shippingApi */
+
+        $shippingApi = Mage::getModel('oggetto_shipping/api_shipping');
+
+        $prices = $shippingApi->calculatePrices($origin, $destination);
+
+        if (!is_array($prices) || $prices === []) {
+            return false;
+        }
+
+        /** @var Mage_Directory_Model_Currency $currency */
+        $currency = Mage::getModel('directory/currency');
+        $currentCurrencyCode = $this->_getCurrentCurrencyCode();
+        $currency->setData(['currency_code' => $currentCurrencyCode]);
+        $apiCurrencyCode = $shippingApi->getCurrency();
+
+        /** @var Mage_Shipping_Model_Rate_Result $rateResult */
+        $rateResult = Mage::getModel('shipping/rate_result');
+
+        foreach ($prices as $method => $price) {
+            if (!array_key_exists($method, $this->getAllowedMethods())) {
+                continue;
+            }
+
+            $price = $this->_reduceAndRoundPrice($price, $apiCurrencyCode, $currentCurrencyCode);
+
+            $rateResult->append($this->_getRateMethod($method, $price));
+        }
+
+        return $rateResult;
 
     }
 
@@ -74,4 +120,116 @@ class Oggetto_Shipping_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstrac
         ];
     }
 
+    /**
+     * Get origin address array (country, region, city) from store config
+     *
+     * @return array
+     */
+    protected function _getOriginAddress()
+    {
+        /** @var  Oggetto_Shipping_Helper_Data $helper */
+        $helper = Mage::helper('oggetto_shipping');
+
+        $originAddress = [
+            'country' => $helper->getShippingOriginCountryId(),
+            'region'  => $helper->getShippingOriginRegionId(),
+            'city'    => $helper->getShippingOriginCity()
+        ];
+
+        return $originAddress;
+    }
+
+    /**
+     * Get destination address array (country, region, city) from request
+     *
+     * @param Mage_Shipping_Model_Rate_Request $request Request
+     * @return array
+     */
+    protected function _getDestinationAddress(Mage_Shipping_Model_Rate_Request $request)
+    {
+        $destinationAddress = [
+            'country' => $request->getDestCountryId(),
+            'region'  => $request->getDestRegionId(),
+            'city'    => $request->getDestCity()
+        ];
+
+        return $destinationAddress;
+    }
+
+    /**
+     * Get region name from region id
+     *
+     * @param string $regionId region id
+     * @return string
+     */
+    protected function _getRegionNameById($regionId)
+    {
+        $directoryModel = Mage::getModel('directory/region');
+
+        $regionName = Mage::helper('oggetto_shipping')->__($directoryModel->load($regionId)->getDefaultName());
+
+        return $regionName;
+    }
+
+    /**
+     * Get country name from country id
+     *
+     * @param string $countryId country id
+     * @return null|string
+     */
+    protected function _getCountryNameById($countryId)
+    {
+        return isset($this->_countryNames[$countryId]) ? $this->_countryNames[$countryId] : null;
+    }
+
+    /**
+     * Get current currency code
+     *
+     * @return string
+     */
+    protected function _getCurrentCurrencyCode()
+    {
+        return Mage::app()->getStore()->getCurrentCurrencyCode();
+    }
+
+    /**
+     * Get rate method
+     *
+     * @param string $method shipping method
+     * @param float  $price  price for shipping
+     *
+     * @return Mage_Shipping_Model_Rate_Result_Method
+     */
+    protected function _getRateMethod($method, $price)
+    {
+        /** @var Mage_Shipping_Model_Rate_Result_Method $rateMethod */
+        $rateMethod = Mage::getModel('shipping/rate_result_method');
+
+        $rateMethod->setCarrier($this->_code);
+        $rateMethod->setCarrierTitle($this->getConfigData('title'));
+        $rateMethod->setMethod($method);
+        $rateMethod->setMethodTitle(ucfirst($method));
+
+        $rateMethod->setPrice($price);
+        $rateMethod->setCost($price);
+
+        return $rateMethod;
+    }
+
+    /**
+     * Reduce money from currency to currency and round it to 2 decimal places
+     *
+     * @param int    $price        reducing price
+     * @param string $fromCurrency from currency
+     * @param string $toCurrency   to currency
+     *
+     * @return float
+     */
+    protected function _reduceAndRoundPrice($price, $fromCurrency, $toCurrency)
+    {
+        $price = Mage::helper('directory')->currencyConvert($price, $fromCurrency, $toCurrency);
+        $price = Mage::app()->getStore()->roundPrice($price);
+
+        return $price;
+    }
 }
